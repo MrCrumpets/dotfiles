bb8c5a9f329ca60bc23a4a6de55407362498b840
Nathan Schucher <nathan.schucher@gmail.com>
2015-12-06 19:58:34 -0500

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function symlinkifne {
+    if [[ -e $1 ]]; then
+        if [[ -L $1 ]]; then
+            echo "$1 already exists"
+            return
+        fi
+        if [[ ! -e ~/.dotfiles_backup/$1 ]]; then
+            mv $1 ~/.dotfiles_backup/
+            echo "backed up $1"
+        fi
+    fi
+    ln -sv .dotfiles/files/$1 ~/$1
+}
+
+function testecho {
+    echo $1
+}
+
+GLOBIGNORE=".:.."
+
+if [[ ! -e ~/.dotfiles_backup ]]; then
+    mkdir ~/.dotfiles_backup
+fi
+
+for file in ~/.dotfiles/files/*; do
+    symlinkifne "${file##*/}"
+done
